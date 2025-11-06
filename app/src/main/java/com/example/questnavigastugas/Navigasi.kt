32be5ec2915df87aa46c97547e262931a4b0e157
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-07 06:26:41 +0700

    add fun HomeApp sebagai fungsi utama yang memanggil semua layar yang ada, memiliki 2 parameter navControler Mengatur rute antar layar lewat NavHost dan modifier untuk mengatur tampilan

--- app/src/main/java/com/example/questnavigastugas/Navigasi.kt
+++ app/src/main/java/com/example/questnavigastugas/Navigasi.kt
@@ -1,7 +1,18 @@
 package com.example.questnavigastugas
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi {
     Beranda,
     Formulir,
     List
 }
+@Composable
+@fun HomeApp(
+    navController: NavHostController= rememberNavController(),
+    modifier: Modifier
+){}
