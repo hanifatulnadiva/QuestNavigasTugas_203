4b18d986ca9851c71116c020c45f19be745ed41e
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-07 06:35:41 +0700

    add Scaffold sebagai layout dasar untuk tampilan android

--- app/src/main/java/com/example/questnavigastugas/Navigasi.kt
+++ app/src/main/java/com/example/questnavigastugas/Navigasi.kt
@@ -1,5 +1,6 @@
 package com.example.questnavigastugas
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
@@ -15,4 +16,6 @@ enum class Navigasi {
 fun HomeApp(
     navController: NavHostController= rememberNavController(),
     modifier: Modifier
-){}
+){
+    Scaffold {  }
+}
