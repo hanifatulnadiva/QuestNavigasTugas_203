a3f4958858793db790ea37c435ea216100c79b2b
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-07 09:41:16 +0700

    mrmbuat file baru lalu membuat function DaftarPeserta dengan parameter OnSubmitBtnClick:()-> Unit

---
+++ app/src/main/java/com/example/questnavigastugas/view/List.kt
@@ -0,0 +1,12 @@
+package com.example.questnavigastugas.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DaftarPeserta(
+    OnSubmitBtnClick:()-> Unit
+){
+
+}
