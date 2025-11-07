6c2eb945d7fb379ec99ba8fd4326c49353bced90
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-07 09:56:12 +0700

    add variabel statis untuk nama, jenis kelamin, status dan juga alamat

--- app/src/main/java/com/example/questnavigastugas/view/List.kt
+++ app/src/main/java/com/example/questnavigastugas/view/List.kt
@@ -2,11 +2,19 @@ package com.example.questnavigastugas.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.questnavigastugas.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DaftarPeserta(
     OnSubmitBtnClick:()-> Unit
 ){
+    val daftar=listOf(
+        Pair(first = stringResource(id=R.string.nama_lengkap), second = "Azira"),
+        Pair(first = stringResource(id=R.string.jenis_kelamin), second = "Perempuan"),
+        Pair(first = stringResource(id=R.string.status), second = "Lajang"),
+        Pair(first = stringResource(id=R.string.alamat), second = "Yogyakarta"),
+    )
 
 }
