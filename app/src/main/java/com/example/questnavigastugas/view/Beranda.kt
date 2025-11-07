3870717f42aaeab76b95c28b166ecc4554d45e14
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-07 08:31:57 +0700

    add layout column di dalam bady Scaffold

--- app/src/main/java/com/example/questnavigastugas/view/Beranda.kt
+++ app/src/main/java/com/example/questnavigastugas/view/Beranda.kt
@@ -1,13 +1,18 @@
 package com.example.questnavigastugas.view
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun welcome(
     OnSubmitBtnClick:()-> Unit
 ){
-    Scaffold {  }
+    Scaffold{ isiRuang->
+        Column {  }
+    }
 }
