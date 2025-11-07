77c708eefb89f17e4ff6ce35140894b2144eb072
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-07 08:47:23 +0700

    add komponen text lalu mengatur ukfont, jenis font, ditulis bold, fontnya warna putih dan mengatur jarak menggunakan spacer

--- app/src/main/java/com/example/questnavigastugas/view/Beranda.kt
+++ app/src/main/java/com/example/questnavigastugas/view/Beranda.kt
@@ -2,13 +2,23 @@ package com.example.questnavigastugas.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.questnavigastugas.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,6 +31,15 @@ fun welcome(
                 .padding(paddingValues = isiRuang),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
-        ){  }
+        ){
+            Text(text= stringResource(id = R.string.welcome), modifier = Modifier,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Serif,
+                fontSize = 40.sp,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(height = 30.dp))
+
+        }
     }
 }
